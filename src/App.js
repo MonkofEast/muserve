a08ae2d236e9a3ebdbb1028109eddac4e748dacf
Qiran Li <qiranli@Qirans-MacBook-Air.local>
2022-11-29 12:29:13 -0500

    added top bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import * as React from 'react';
-import NaviBar from './components/bottom-navigation/NaviBar';
-
+import NaviBar from './components/bottomNavigation/NaviBar';
+import TopBar from './components/topBar/TopBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <TopBar />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
